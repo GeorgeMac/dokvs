0803feb008ad3edbcaaf5b7f797a6e44d22ab3fb
George MacRorie <me@georgemac.com>
2022-01-23 13:58:22 +0000

    feat: add support for generic document schema

--- document.go
+++ document.go
@@ -2,44 +2,49 @@ package etcdoc
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
-    "encoding/json"
 )
 
 var ErrNotFound = errors.New("document not found")
 
 type Serializer[T any] interface {
 	Serialize(T) ([]byte, error)
-    Deserialize([]byte, *T) error
+	Deserialize([]byte, *T) error
 }
 
-type JSONSerializer[T any] struct {}
+type JSONSerializer[T any] struct{}
 
 func (s JSONSerializer[T]) Serialize(t T) ([]byte, error) {
-    return json.Marshal(t)
+	return json.Marshal(t)
 }
 
 func (d JSONSerializer[T]) Deserialize(v []byte, t *T) error {
-    return json.Unmarshal(v, t)
+	return json.Unmarshal(v, t)
 }
 
-type Document interface {
-	PrimaryKey() []byte
+type Schema[D any] interface {
+	Collection() []byte
+    PrimaryKey(D) []byte
 }
 
-type Collection[D Document] struct {
-	// client clientv3.KV
+type Collection[D any] struct {
 	kv         KV
+	schema     Schema[D]
 	serializer Serializer[D]
 }
 
-func (c *Collection[D]) Fetch(ctx context.Context, key []byte) (d D, err error) {
-    item, err := c.kv.Fetch(ctx, key)
-    if err != nil {
-        return d, err
-    }
+func NewCollection[D any](kv KV, schema Schema[D], serializer Serializer[D]) Collection[D] {
+    return Collection[D]{kv: kv, schema: schema, serializer: serializer}
+}
 
-    err = c.serializer.Deserialize(item.V, &d)
+func (c *Collection[D]) Fetch(ctx context.Context, key []byte) (d D, err error) {
+	item, err := c.kv.Fetch(ctx, key)
+	if err != nil {
+		return d, err
+	}
+
+	err = c.serializer.Deserialize(item.V, &d)
 
 	return
 }
@@ -54,14 +59,14 @@ func (c *Collection[D]) List(context.Context, ...ListPredicate) (d D, err error)
 }
 
 func (c *Collection[D]) Put(ctx context.Context, doc D) error {
-    v, err := c.serializer.Serialize(doc)
-    if err != nil {
-        return err
-    }
+	v, err := c.serializer.Serialize(doc)
+	if err != nil {
+		return err
+	}
 
-	return c.kv.Put(ctx, doc.PrimaryKey(), v)
+	return c.kv.Put(ctx, c.schema.PrimaryKey(doc), v)
 }
 
 func (c *Collection[D]) Delete(ctx context.Context, doc D) error {
-	return c.kv.Delete(ctx, doc.PrimaryKey())
+	return c.kv.Delete(ctx, c.schema.PrimaryKey(doc))
 }
--- example_test.go
+++ example_test.go
@@ -1,7 +1,19 @@
 package etcdoc
 
-type Recipe struct{}
+type Recipe struct {
+	ID string
+}
+
+type RecipeSchema struct{}
+
+func (RecipeSchema) Collection() []byte { return []byte("recipes") }
+
+func (RecipeSchema) PrimaryKey(r Recipe) []byte {
+	return []byte(r.ID)
+}
 
 func ExampleCollection() {
-	recipes := Collectionp[Recipe]{}
+	var kv KV
+	recipes := NewCollection(kv, Schema[Recipe](RecipeSchema{}), Serializer[Recipe](JSONSerializer[Recipe]{}))
+	_ = recipes
 }
